1c5f8f4435773687d47f094f20a21da975a5aeb4
Remi van Deursen <cryslo1994@gmail.com>
2018-03-14 23:21:33 +0100

    Added factory and grid

---
+++ Assets/Data/Scripts/Grid.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour {
+    public GameObject tile;
+    private int xLength = 20;
+    private int yLength = 20;
+    private int xPos = -50;
+    private int yPos = 0;
+    private GameObject[,] tiles;
+	// Use this for initialization
+	void Start () {
+        for (int x = 0; x < xLength; x++)
+        {
+            for (int y = 0; y < yLength; y++)
+            {
+                tiles[x,y] = (GameObject)Instantiate(tile, new Vector3(xPos,0,yPos), Quaternion.identity);
+                yPos += 5;
+            }
+            xPos += 5;
+            yPos = 0;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
